9a4cfd42850dd8f7a1b0fda96c45d0aa42d53c19
Prem Buddhbhatti <premkansara9@gmail.com>
2022-04-05 18:56:03 +0530

    mvc added

--- src/admin.js
+++ src/admin.js
@@ -1,30 +1,29 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3001;
 
-const publicDirectory = path.join(__dirname, '../public')
-const viewPath = path.join(__dirname, 'templates/views/admin')
-const partialsPath = path.join(__dirname, 'templates/partials/admin')
-console.log(__dirname)
-console.log(publicDirectory)
-app.set('view engine', 'hbs')
-app.set('views', viewPath)
-hbs.registerPartials(partialsPath)
+const publicDirectory = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "templates/views/admin");
+const partialsPath = path.join(__dirname, "templates/partials/admin");
+console.log(__dirname);
+console.log(publicDirectory);
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
 
+app.use(express.static(publicDirectory));
 
-app.use(express.static(publicDirectory))
-
-app.get('/admin', (req, res) => {
-    res.render('index', {
-            title: 'This is Admin Page',
-            name: 'Kishan Patel'
-        })
-        // res.send("Hello")
-})
+app.get("/admin", (req, res) => {
+	res.render("index", {
+		title: "This is Admin Page",
+		name: "Kishan Patel",
+	});
+	// res.send("Hello")
+});
 
 app.listen(port, () => {
-    console.log("Server Started")
-})
+	console.log("Server Started");
+});
