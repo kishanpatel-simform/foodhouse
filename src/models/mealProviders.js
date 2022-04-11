5037eea16072e43844938a72dd07b58398c0eaef
Prem Buddhbhatti <premkansara9@gmail.com>
2022-04-11 19:11:48 +0530

    modeules updated

--- src/models/mealProviders.js
+++ src/models/mealProviders.js
@@ -2,28 +2,28 @@ const mongoose = require("mongoose");
 const validate = require("validator");
 
 const mealProvidersSchema = new mongoose.Schema({
-  orgName: {
-    type: String,
-    require: true,
-    trim: true,
-  },
-  isActive: {
-    type: Boolean,
-    require: true,
-    default: true,
-  },
-  isVerified: {
-    type: Boolean,
-    require: true,
-    default: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "userSchema",
-  },
+	orgName: {
+		type: String,
+		require: true,
+		trim: true,
+	},
+	isActive: {
+		type: Boolean,
+		require: true,
+		default: true,
+	},
+	isVerified: {
+		type: Boolean,
+		require: true,
+		default: true,
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "userSchema",
+	},
 });
 
 const mealProviders = mongoose.model("mealProviders", mealProvidersSchema);
 
-modules.exports = mealProviders;
+module.exports = mealProviders;
